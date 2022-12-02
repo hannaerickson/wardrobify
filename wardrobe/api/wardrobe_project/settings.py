a94e206c0c2563176c97ec4dc6c12d3f3cf2763c
Ryan Holzer <ryanjames.holzer@gmail.com>
2022-12-02 13:02:49 -0700

    added HatsApiConfig

--- wardrobe/api/wardrobe_project/settings.py
+++ wardrobe/api/wardrobe_project/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = ["wardrobe-api", 'localhost']
 # Application definition
 
 INSTALLED_APPS = [
+    "hats.api.hats_rest.apps.HatsApiConfig",
     "shoes.api.shoes_rest.apps.ShoesApiConfig",
     "corsheaders",
     "wardrobe_api.apps.WardrobeApiConfig",
