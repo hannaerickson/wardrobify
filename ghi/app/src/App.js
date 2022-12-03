f9824fa5c8cd8f1d51950d4537e6ebb8b40a4633
Ryan Holzer <ryanjames.holzer@gmail.com>
2022-12-03 08:45:57 -0700

    created base files for components and added to Nav

--- ghi/app/src/App.js
+++ ghi/app/src/App.js
@@ -9,6 +9,8 @@ function App() {
       <div className="container">
         <Routes>
           <Route path="/" element={<MainPage />} />
+          <Route path="/shoes" element={<ShoesList />} />
+          <Route path="/hats" element={<HatsList />} />
         </Routes>
       </div>
     </BrowserRouter>
---
+++ ghi/app/src/HatsList.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+class HatsList extends React.Component {
+
+}
+
+export default HatsList
